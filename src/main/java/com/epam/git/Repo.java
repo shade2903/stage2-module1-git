dab46aefaa403d717771c5d591934c7fbd5a7e39
Vladimir Haiduk <nightshade2903@gmail.com>
2023-05-13 22:33:16 +0300

    commit

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/shade2903/mjsGitTask.git";
+    public static String REPO_LINK = "https://github.com/shade2903/mjsGitTask2.git";
 }
